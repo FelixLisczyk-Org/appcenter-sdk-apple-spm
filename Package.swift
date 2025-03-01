0d66f95a4bc8490925a133c06e19120cae47be3e
Felix Lisczyk <5102728+FelixLisczyk@users.noreply.github.com>
2025-03-01 09:51:40 +0100

    Updated AppCenter framework

--- Package.swift
+++ Package.swift
@@ -25,13 +25,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AppCenter",
-            url: "https://www.snipnotes.de/files/app-center/v5.0.5/AppCenter.xcframework.zip",
-            checksum: "4b1df69e60f691a4191abca62c4501a5d98cf6fc8542c57201b39178b7b71a5c"
+            url: "https://www.snipnotes.de/files/app-center/v5.0.6/AppCenter.xcframework.zip",
+            checksum: "c48a9e3fbec521570466f1884cba60a8e9a7051b6e1f4cc3279755a447678a98"
         ),
         .binaryTarget(
             name: "AppCenterCrashes",
-            url: "https://www.snipnotes.de/files/app-center/v5.0.5/AppCenterCrashes.xcframework.zip",
-            checksum: "a3e386d426f91ca383ef2bf402562d00fb6b28b9f83cecc41a92395322d0024e"
+            url: "https://www.snipnotes.de/files/app-center/v5.0.6/AppCenterCrashes.xcframework.zip",
+            checksum: "d6f0ad9f4156a40c5b8e08d0778b87a22ade9919338677ff4919881a500734c3"
         ),
         .target(
             name: "AppCenterTarget",
